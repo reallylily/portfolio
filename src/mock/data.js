91ae88741ac6f2a7b25a214bc2d2cce907b9704e
jitsumacmaster <jitsu.macmaster@gmail.com>
2020-06-26 15:05:29 -0700

    Init

---
+++ src/mock/data.js
@@ -0,0 +1,94 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: '',
+  subtitle: '',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: uuidv4(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv4(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv4(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: '',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: uuidv4(),
+      name: 'twitter',
+      url: '',
+    },
+    {
+      id: uuidv4(),
+      name: 'codepen',
+      url: '',
+    },
+    {
+      id: uuidv4(),
+      name: 'linkedin',
+      url: '',
+    },
+    {
+      id: uuidv4(),
+      name: 'github',
+      url: '',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
