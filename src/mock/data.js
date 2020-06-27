2e2f68a90f247dbf2ec7b41e7ca1482da76cdcfc
jitsumacmaster <jitsu.macmaster@gmail.com>
2020-06-26 17:29:52 -0700

    Add Babble image

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,16 +2,16 @@ import { v4 as uuidv4 } from 'uuid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Jitsu MacMaster | Software Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Jitsu MacMaster',
+  subtitle: "I am a Software Engineer with a passion for code with a hint of social justice!",
   cta: '',
 };
 
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://docs.google.com/document/d/1OOKLbng1Y_2EDzyDqxbFi5h-oCzEGvDNUHBFKxGGTsY/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,20 +29,20 @@ export const projectsData = [
   {
     id: uuidv4(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Death Star Trench',
+    info: 'Fly an X-Wing down the infamous Deathstar Trench, and take your shot at blowing up the Deathstar. May the force be with you!',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://jitsumacmaster.github.io/Deathstar-Run/dist/',
+    repo: 'https://github.com/jitsumacmaster/Deathstar-Run', // if no repo, the button will not show up
   },
   {
     id: uuidv4(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'BabblChatLogo.jpg',
+    title: 'BabbleChat',
+    info: 'At BabbleChat, we strive to make learning languages an enjoyable experience for all language learners, from the conversational newbie to the linguistic expert, to the dabbling hobbyist in between. Through our chat dynamic platform, connect with people from across the world. Master fluency in a language, one word a time, with exciting conversational partners along the way.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'http://babble-chat-app.herokuapp.com/#/',
+    repo: 'https://github.com/jitsumacmaster/BabbleChat', // if no repo, the button will not show up
   },
   {
     id: uuidv4(),
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'jitsu.macmaster@gmail.com',
 };
 
 // FOOTER DATA
